009d2f0cf20c0be5377f7bdda9ed2f1e54a7f38a
Justin Kian Romeon <romeonjustin@gmail.com>
2025-03-22 23:33:44 +0800

    Add Task data model

---
+++ src/Tasks.hs
@@ -0,0 +1,33 @@
+module Tasks where
+
+import Data.Time.Calendar (Day)
+import Text.Printf (printf)
+
+data Priority = Low 
+              | Medium 
+              | High 
+              deriving (Show, Enum)
+
+data Status = Todo 
+            | Done 
+            deriving (Show, Enum)
+
+type TaskId = Int
+type Description = String
+
+data Task = Task
+  { taskId :: Int
+  , description :: Description
+  , completed :: Status
+  , priority :: Priority
+  , dueDate :: Maybe Day
+  }
+
+instance Show Task where
+  show (Task tId desc comp prio due) =
+    printf "%-4d | %-40s | %-9s | %-8s | %-10s"
+      tId
+      desc
+      (show comp)
+      (show prio)
+      (show due)
