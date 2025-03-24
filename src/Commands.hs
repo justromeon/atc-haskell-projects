e987b0d18a0b390f710d7097921b6a9daf639c47
Justin Kian Romeon <romeonjustin@gmail.com>
2025-03-24 20:56:17 +0800

    Improve command parsers' robustness + better messages, and rename Exit command to Quit

--- src/Commands.hs
+++ src/Commands.hs
@@ -1,6 +1,7 @@
 module Commands where
 
 import Data.Functor (($>))
+import Data.Maybe (isJust)
 import Data.Time.Calendar (Day, fromGregorianValid)
 import Text.Parsec
 import Text.Parsec.String (Parser)
@@ -14,62 +15,75 @@ data Command
   | DeleteTask TaskId
   | EditTask TaskId Description
   | Help
-  | Exit
+  | Quit
   deriving (Show)
 
 commandParser :: Parser Command
-commandParser =
-  addTaskParser
-  <|> viewParser
-  <|> markParser
-  <|> deleteParser
-  <|> editParser
-  <|> helpParser
-  <|> exitParser
+commandParser = do
+  spaces
+  cmd <- addTaskParser
+     <|> viewParser
+     <|> markParser
+     <|> deleteParser
+     <|> editParser
+     <|> helpParser
+     <|> exitParser
+  spaces
+  eof
+  return cmd
 
 addTaskParser :: Parser Command
 addTaskParser = do
   string "add" *> spaces
-  desc <- between (char '"') (char '"') (many1 anyChar)
-  pri <- optionMaybe $ try (spaces *> string "-p" *> priorityParser)
-  due <- optionMaybe $ try (spaces *> string "-d" *> spaces *> dateParser)
+  desc  <- between (char '"') (char '"') (many1 (noneOf "\""))
+ 
+  -- Prevent "-" consumption if only "-d" follows.
+  pFlag <- optionMaybe $ try (spaces *> string "-p")
+
+  -- Isolate priority parsing to avoid masking priority ParseError with "try".
+  pri <- if isJust pFlag
+    then optionMaybe (spaces *> priorityParser)
+    else return Nothing
+
+  due <- optionMaybe (spaces *> string "-d" *> many1 space *> dateParser)
   return $ AddTask desc pri due
 
 viewParser :: Parser Command
 viewParser = do
   string "view" *> spaces
-  select <- optionMaybe (try (string "-i") <|> string "-c")
+  select <- optionMaybe (char '-' *> (char 'i' <|> char 'c'))
   case select of
-    Just "-i" -> return $ ViewTasks (Just Todo)
-    Just "-c" -> return $ ViewTasks (Just Done)
-    Nothing   -> return $ ViewTasks Nothing
+    Just 'i' -> return $ ViewTasks (Just Todo)
+    Just 'c' -> return $ ViewTasks (Just Complete)
+    _        -> return $ ViewTasks Nothing
 
 markParser :: Parser Command
 markParser = do
   string "mark" *> spaces
-  taskId <- read <$> many1 digit
-  status <- (string "todo" $> Todo) <|> (string "done" $> Done)
+  taskId <- read <$> many1 digit <?> "taskId"
+  spaces
+  status <- (string "todo" $> Todo) <|> (string "comp" $> Complete)
   return $ MarkTask taskId status
 
 deleteParser :: Parser Command
 deleteParser = do
   string "delete" *> spaces
-  taskId <- read <$> many1 digit
+  taskId <- read <$> many1 digit <?> "taskId"
   return $ DeleteTask taskId
 
 editParser :: Parser Command
 editParser = do
   string "edit" *> spaces
-  taskId <- read <$> many1 digit
+  taskId <- read <$> many1 digit <?> "taskId"
   spaces
-  desc <- between (char '"') (char '"') (many1 anyChar)
+  desc <- between (char '"') (char '"') (many1 (noneOf "\"")) <?> "quotation mark"
   return $ EditTask taskId desc
 
 helpParser :: Parser Command
 helpParser = string "-h" $> Help
 
 exitParser :: Parser Command
-exitParser = string "exit" $> Exit
+exitParser = string "quit" $> Quit
 
 priorityParser :: Parser Priority
 priorityParser = (string "high" $> High)
@@ -78,11 +92,11 @@ priorityParser = (string "high" $> High)
 
 dateParser :: Parser Day
 dateParser = do
-  y <- many1 digit
+  y <- count 4 digit
   char '-'
-  m <- many1 digit
+  m <- count 2 digit
   char '-'
-  d <- many1 digit
+  d <- count 2 digit
   case fromGregorianValid (read y) (read m) (read d) of
     Just day -> return day
-    Nothing  -> fail "Invalid date"
+    Nothing  -> fail "Invalid date! Use this format: YYYY-MM-DD (without surrounding quot marks)"
