f6be5bce430edaf37476234f41cd71fe93ea9d72
Justin Kian Romeon <romeonjustin@gmail.com>
2025-04-19 12:35:55 +0800

    Add user interaction

--- src/Main.hs
+++ src/Main.hs
@@ -1,26 +1,52 @@
+{-# LANGUAGE OverloadedStrings #-}
 module Main where
 
-import System.IO (hFlush, stdout)
+import Database.SQLite.Simple (close, execute_, open, Query)
+import Text.Parsec (parse)
+
+import Commands
+import Database
 
 main :: IO ()
 main = do
-  putStrLn "Welcome to my TODO List Manager!"
-  loop
-
-loop :: IO ()
-loop = do
-  putStr "Enter command: "
-  hFlush stdout
+  putStrLn "Enter a command. Use -h for help."
   input <- getLine
-  isLooping <- handleInput input
-  if isLooping
-    then loop
-    else return ()
+  processInput input
 
-handleInput :: String -> IO Bool
-handleInput "exit" = do
-  putStrLn "Goodbye!"
-  pure False
-handleInput input = do
-  putStrLn $ "You entered: " ++ input
-  pure True
+processInput :: String -> IO ()
+processInput input =
+  case parse commandParser "" input of
+    Left err   -> do putStrLn $ "\n" ++ show err ++ "\n"
+                     main
+    Right Quit -> putStrLn "Closing Todo App.. Thankyou!"
+    Right cmd  -> do executeCommand cmd
+                     main
+
+executeCommand :: Command -> IO ()
+executeCommand cmd = do
+  conn <- open "todo.db"
+  execute_ conn schema
+  case cmd of
+    AddTask desc prioLevel dueDate -> insertTask conn desc prioLevel dueDate
+    MarkTask taskId status         -> updateTaskMark conn taskId status
+    ViewTasks filterMark           -> fetchTasks conn filterMark
+    DeleteTask taskId              -> deleteTask conn taskId
+    EditTask taskId newDesc        -> updateTaskDesc conn taskId newDesc
+    Help                           -> displayHelpMenu
+  close conn
+
+displayHelpMenu :: IO ()
+displayHelpMenu = putStrLn $ unlines [
+    "Available commands:",
+    "  add \"description\" [-p priority] [-d yyyy-mm-dd] - Add a new task.",
+    "    -p: Priority (high, medium, low). Optional.",
+    "    -d: Due date (yyyy-mm-dd). Optional.",
+    "  view [-i|-c] - View tasks.",
+    "    -i: View incomplete tasks.",
+    "    -c: View completed tasks.",
+    "  mark <taskId> <todo|comp> - Mark a task as todo or complete.",
+    "  delete <taskId> - Delete a task.",
+    "  edit <taskId> \"new description\" - Edit a task's description.",
+    "  -h - Display this help menu.",
+    "  quit - Quit the application."
+  ]
