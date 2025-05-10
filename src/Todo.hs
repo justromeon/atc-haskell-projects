3169e286a04ec55251f8f026fa27b939a4a435eb
Justin Kian Romeon <romeonjustin@gmail.com>
2025-05-10 19:56:06 +0800

    Add smart constructors

--- src/Todo.hs
+++ src/Todo.hs
@@ -1,7 +1,9 @@
 module Todo where
 
-import Data.Text (Text)
+import Data.Char (toLower)
+import Data.Text (Text, pack)
 import Data.Time.Calendar (Day)
+import Data.Time (defaultTimeLocale, parseTimeM)
 
 --Domain Model
 newtype TaskId = TaskId Int
@@ -23,3 +25,29 @@ data Task = Task
     , priority    :: Priority
     , dueDate     :: Maybe Day
     } deriving Show
+
+--Smart Constructors
+mkDesc :: String -> Either String Description
+mkDesc s
+    | null s    = Left "Description can't be empty."
+    | otherwise = Right $ Description $ pack s
+
+mkStatus :: String -> Either String Status
+mkStatus s = case map toLower s of
+  "incomplete" -> Right Incomplete
+  "complete"   -> Right Complete
+  _            -> Left $ "Invalid Status: " ++ s ++ " must be 'incomplete' or 'complete'"
+
+mkPriority :: String -> Either String Priority
+mkPriority s = case map toLower s of
+    "none"   -> Right None
+    "low"    -> Right Low
+    "medium" -> Right Medium
+    "high"   -> Right High
+    _        -> Left $ "Invalid priority: '" ++ s ++ "'. Must be one of: none, low, medium, high."
+
+parseDueDate :: String -> Either String Day
+parseDueDate s =
+    case parseTimeM True defaultTimeLocale "%Y-%m-%d" s of
+        Just day -> Right day
+        Nothing  -> Left $ "Invalid date format: '" ++ s ++ "'. Please use YYYY-MM-DD."
