394e4c19d59294747f262b6088cc1564a656ecc4
Justin Kian Romeon <romeonjustin@gmail.com>
2025-06-15 21:11:33 +0800

    feat: connect command parser to DB operations

--- src/Main.hs
+++ src/Main.hs
@@ -1,8 +1,24 @@
 module Main where
 
+import Database.SQLite.Simple
 import Options.Applicative (execParser)
 
 import CLI
+import Database
+
+executeCommand :: Command -> IO ()
+executeCommand cmd = do
+    conn <- open "todo.db"
+    execute_ conn schema
+    case cmd of
+        AddTask desc prio day -> insertTask conn desc prio day
+        ViewTasks stat sorter -> displayTasks conn stat sorter
+        CompleteTask taskId   -> setTaskComplete conn taskId
+        DeleteTask taskId     -> deleteTask conn taskId
+        EditTask taskId d s p due -> updateTask conn taskId d s p due 
+        Quit                  -> putStrLn "Closing the Todo App... Thankyou!"
+    close conn
 
 main :: IO ()
-main = execParser commandParserInfo >>= print 
+main = execParser commandParserInfo >>= executeCommand
+     
